6303a7c8dd0f2c3db94bd841bd98c58a25c7af46
Benjamin Dore <bjdore@svsu.edu>
2021-01-30 15:35:07 -0500

    Sidebar styling improvements. Will stay highlighted on page selection.

--- DirectorsPortalWPF/MainWindow.xaml.cs
+++ DirectorsPortalWPF/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using DirectorsPortalWPF.SettingsUI;
 using DirectorsPortalWPF.TodoUI;
 using DirectorsPortalWPF.ValidateWebsite;
 using System.Windows;
+using System.Windows.Media;
 
 /// <summary>
 /// 
@@ -54,36 +55,92 @@ namespace DirectorsPortalWPF
         private void PaymentsPage_Navigate(object sender, RoutedEventArgs e)
         {
             mainFrame.Navigate(new PaymentsPage());
+
+            btnSettings.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnEmail.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnGenReport.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnMember.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));       
+            btnPayment.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFD8D8D8"));      // Appears selected
+            btnTodo.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnValWeb.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
         }
 
         private void MembersPage_Navigate(object sender, RoutedEventArgs e)
         {
             mainFrame.Navigate(new MembersPage());
+
+            btnSettings.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnEmail.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));        
+            btnGenReport.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnMember.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFD8D8D8"));       // Appears selected
+            btnPayment.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnTodo.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnValWeb.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
         }
 
         private void EmailPage_Navigate(object sender, RoutedEventArgs e)
         {
             mainFrame.Navigate(new EmailPage());
+
+            btnSettings.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnEmail.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFD8D8D8"));        // Appears selected
+            btnGenReport.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnMember.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnPayment.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnTodo.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnValWeb.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
         }
 
         private void WebsitePreviewPage_Navigate(object sender, RoutedEventArgs e)
         {
             mainFrame.Navigate(new WebsitePreviewPage());
+
+            btnSettings.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnEmail.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnGenReport.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnMember.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnPayment.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnTodo.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnValWeb.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFD8D8D8"));       // Appears selected
         }
 
         private void TodoPage_Navigate(object sender, RoutedEventArgs e)
         {
             mainFrame.Navigate(new TodoPage());
+
+            btnSettings.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnEmail.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnGenReport.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnMember.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnPayment.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnTodo.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFD8D8D8"));         // Appears selected
+            btnValWeb.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
         }
 
         private void SettingsPage_Navigate(object sender, RoutedEventArgs e)
         {
             mainFrame.Navigate(new SettingsPage());
+
+            btnSettings.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFD8D8D8"));     // Appears selected
+            btnEmail.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnGenReport.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnMember.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnPayment.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnTodo.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnValWeb.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
         }
 
         private void GenerateReportsPage_Navigate(object sender, RoutedEventArgs e)
         {
             mainFrame.Navigate(new GenerateReportsPage());
+
+            btnSettings.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));     
+            btnEmail.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnGenReport.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFD8D8D8"));    // Appears selected
+            btnMember.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnPayment.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnTodo.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
+            btnValWeb.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F2F7"));
         }
     }
 }
