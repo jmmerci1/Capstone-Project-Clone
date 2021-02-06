bfa9291ddbfe7d50d2e9ca4046ebd1ed8154ea55
Morgan Simmons <mbsimmon@svsu.edu>
2021-02-05 20:38:30 -0500

    GET, POST, and PUT for Email Campaigns

---
+++ DirectorsPortal/ConstantContactAPIObjects/EmailCampaigns/GETEmailCampaigns.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectorsPortal
+{
+    class GETEmailCampaigns
+    {
+        public string campaign_id { get; set; }
+        public DateTime created_at { get; set; }
+        public string current_status { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public DateTime updated_at { get; set; }
+    }
+}
