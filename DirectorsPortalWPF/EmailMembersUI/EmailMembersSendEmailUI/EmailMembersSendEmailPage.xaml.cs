54b0124300828d7d65d09c37516b30ad5b1070e4
Benjamin Dore <bjdore@svsu.edu>
2021-02-27 22:26:29 +0000

    Lexical analyzer from Microsoft that converts the XAML in a FlowDocument to HTML. Created a method to extract FlowDocument to XML.

--- DirectorsPortalWPF/EmailMembersUI/EmailMembersSendEmailUI/EmailMembersSendEmailPage.xaml.cs
+++ DirectorsPortalWPF/EmailMembersUI/EmailMembersSendEmailUI/EmailMembersSendEmailPage.xaml.cs
@@ -1,6 +1,8 @@
 using DirectorsPortal;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +11,12 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
 /// <summary>
 /// This file has all of the logic for handling the email page.
@@ -27,6 +31,8 @@ namespace DirectorsPortalWPF.EmailMembersSendEmailUI
     /// </summary>
     public partial class EmailMembersSendEmailPage : Page
     {
+        private string gStrAttachedFilePath;
+
         public EmailMembersSendEmailPage()
         {
             InitializeComponent();
@@ -45,11 +51,20 @@ namespace DirectorsPortalWPF.EmailMembersSendEmailUI
         {
             String strSubject = txtSubject.Text;
             String strRecipient = txtToField.Text;
-            String[] arrRecipient = strRecipient.Split(';');
+            String[] rgRecipient = strRecipient.Split(';');
 
-            String strContent = txtBody.Text;
+            String strContent = GetRichTextDocumentHtmlContent();
 
-            await GraphApiClient.SendMail(strSubject, arrRecipient, strContent);
+            await GraphApiClient.SendMail(strSubject, rgRecipient, strContent);
+
+            txtToField.Clear();
+            txtSubject.Clear();
+            rtbEmailBody.Document.Blocks.Clear();
+
+            if (rgRecipient.Length > 1)
+                MessageBox.Show($"Message sent to {rgRecipient[0]} and {rgRecipient.Length - 1} others.", "Message Sent");
+            else
+                MessageBox.Show($"Message sent to {rgRecipient[0]}.", "Message Sent");
         }
         /// <summary>
         /// empty method for use later 
@@ -68,9 +83,59 @@ namespace DirectorsPortalWPF.EmailMembersSendEmailUI
         /// </summary>
         private async void GetEmail()
         {
+            // TODO: Still needs to be implemented
+            // Need API to call from the SDK team
             String strID = "";
-
             await GraphApiClient.GetEmail(strID);
+
+        }
+
+        /// <summary>
+        /// Get the FlowDocument content of the RichTextBox field containing the body of the email and convert
+        /// it to an HTML document that can be read by the GraphApiClient
+        /// </summary>
+        /// <returns>A string containing the HTML for the RichTextField content</returns>
+        private string GetRichTextDocumentHtmlContent()
+        {
+            FlowDocument fdRichTextBoxBody = rtbEmailBody.Document;
+            XmlDocument xDocFlowDocToXml = new XmlDocument();
+
+            xDocFlowDocToXml.LoadXml(XamlWriter.Save(fdRichTextBoxBody));
+
+            StringWriter swString = new StringWriter();
+            XmlTextWriter xwXml = new XmlTextWriter(swString);
+            xDocFlowDocToXml.WriteTo(xwXml);
+
+            return HtmlFromXamlConverter.ConvertXamlToHtml(swString.ToString());
+        }
+
+        /// <summary>
+        /// Button intended to attach files to an email message. Assigns filepath of the attachment to a global
+        /// string.
+        /// </summary>
+        /// <param name="sender">The 'Attach' button</param>
+        /// <param name="e">The Click event</param>
+        private void AttachFile_Click(object sender, RoutedEventArgs e)
+        {
+            gStrAttachedFilePath = OpenFile();
+            MessageBox.Show(gStrAttachedFilePath);
+
+        }
+
+        /// <summary>
+        /// Allows the user to select a file using a FileDialog window. Gets the selected
+        /// file's path.
+        /// </summary>
+        /// <returns>A file path as a string</returns>
+        private string OpenFile()
+        {
+            string strFilePath = "";
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+                strFilePath = openFileDialog.FileName;
+
+            return strFilePath;
         }
     }
 }
